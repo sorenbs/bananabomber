d2bd25ce16790b8df437066bb8fcd9c8f42b7dce
sorenbs <sorenbs@gmail.com>
2011-04-03 22:10:07 +0200

    bombs destroy flowers. use infinite animate instead of onenterframe. Much faster.

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     //start crafty
-    Crafty.init(50, 400, 320);
+    Crafty.init(50, 400, 336);
     Crafty.canvas();
 
     //turn the sprite map into usable components
@@ -21,35 +21,54 @@ window.onload = function () {
         //generate the grass along the x-axis
         for (var i = 0; i < 25; i++) {
             //generate the grass along the y-axis
-            for (var j = 0; j < 20; j++) {
+            for (var j = 0; j < 21; j++) {
                 grassType = Crafty.randRange(1, 4);
                 Crafty.e("2D, canvas, grass" + grassType)
-                        .attr({ x: i * 16, y: j * 16 });
+                        .attr({ x: i * 16, y: j * 16, z:1 });
 
                 //1/50 chance of drawing a flower and only within the bushes
-                if (i > 0 && i < 24 && j > 0 && j < 19 && Crafty.randRange(0, 50) > 49) {
-                    Crafty.e("2D, DOM, flower, animate")
+                if (i > 0 && i < 24 && j > 0 && j < 19 && Crafty.randRange(0, 50) > 5) {
+                    var f = Crafty.e("2D, DOM, flower, animate, explodable")
                             .attr({ x: i * 16, y: j * 16, z: 1000 })
                             .animate("wind", 0, 1, 3)
-                            .bind("enterframe", function () {
-                        if (!this.isPlaying())
-                            this.animate("wind", 80);
-                    });
+                            //.bind("enterframe", function () {
+                            //   if (!this.isPlaying())
+                            //        this.animate("wind", 80);
+                            //})
+                            .bind('explode', function() {
+                                this.destroy();
+                            });
+
+//                    if (!f.isPlaying())
+  //                      f.animate("wind", 80);
+
+
+                    ani.call(f);
+                    function ani() {
+                        this.animate('wind', 80, -1);
+                        //this.delay(ani, 80);
+                    };
+                }
+
+                //Gitter
+                if((i % 2 === 0) && (j % 2 === 0)) {
+                    Crafty.e("2D, Grid, canvas, wall, bush1")
+                        .attr({ col: i, row: j, z: 2000});
                 }
             }
         }
 
         //create the bushes along the x-axis which will form the boundaries
         for (var i = 0; i < 25; i++) {
-            Crafty.e("2D, canvas, wall_top, wall, bush" + Crafty.randRange(1, 2))
+            Crafty.e("2D, canvas, wall, bush" + Crafty.randRange(1, 2))
                     .attr({ x: i * 16, y: 0, z: 2 });
-            Crafty.e("2D, DOM, wall_bottom, wall, bush" + Crafty.randRange(1, 2))
-                    .attr({ x: i * 16, y: 304, z: 2 });
+            Crafty.e("2D, DOM, wall, bush" + Crafty.randRange(1, 2))
+                    .attr({ x: i * 16, y: 320, z: 2 });
         }
 
         //create the bushes along the y-axis
         //we need to start one more and one less to not overlap the previous bushes
-        for (var i = 1; i < 19; i++) {
+        for (var i = 1; i < 20; i++) {
             Crafty.e("2D, DOM, wall_left, wall, bush" + Crafty.randRange(1, 2))
                     .attr({ x: 0, y: i * 16, z: 2 });
             Crafty.e("2D, canvas, wall_right, wall, bush" + Crafty.randRange(1, 2))
@@ -66,7 +85,7 @@ window.onload = function () {
 
         //black background with some loading text
         Crafty.background("#000");
-        Crafty.e("2D, DOM, text").attr({ w: 100, h: 20, x: 150, y: 120 })
+        Crafty.e("2D, DOM, Text").attr({ w: 100, h: 20, x: 150, y: 120 })
                 .text("Loading")
                 .css({ "text-align": "center" });
     });
@@ -74,87 +93,118 @@ window.onload = function () {
     //automatically play the loading scene
     Crafty.scene("loading");
 
-    Crafty.scene("main", function () {
-        generateWorld();
+    /*Please note that this component is a bit over engineered for our purpose. This is because i am an engineer at
+    heart and really enjoy building reusable components. Best practice is to build components just sophisticated enough
+    to meet current requirements and then extend them when new use cases emerge.*/
+    Crafty.c('Dropper', {
+        _drop: false,
+        _dropType: 'default',
+        _key: Crafty.keys.SPACE,
 
 
+        Dropper: function(dropKey, dropType) {
+            if (dropKey) this._key = dropKey;
+            if(dropType) this._dropType = dropType;
 
-        Crafty.c('Droppable', {
-            _drop: false,
-            _key: Crafty.keys.SPACE,
-
-
-            Droppable: function(dropKey) {
-                if (dropKey) this._key = dropKey;
-
-                this.bind('enterframe',
-                        function (e) {
-                            if (this._drop) {
-                                this._drop = false;
-
-                                Crafty.e("2D, canvas, animate, Grid, droppable, banana")
-                                        .attr({ col: player.col, row:player.row, z:100})
-                                        .animate('explode', 4, 0, 5)
-                                        .bind('enterframe', function () {
-                                    if (!this.isPlaying())
-                                        this.animate('explode', 50);
-                                })
-                                        .delay(function() {
-                                    this.destroy();
-                                }, 3000);
-                            }
-                        }).bind('keydown', function (e) {
+            return this;
+        },
+        init: function() {
+            this.bind('enterframe',
+                function (e) {
+                    if (this._drop) {
+                        this._drop = false;
+                        this.trigger('Dropped', {dropType: this._dropType});
+                    }
+                })
+                .bind('keydown', function (e) {
                     if (e.keyCode === this._key) this._drop = true;
                 });
+        }
+    });
 
-                return this;
-            }
+    Crafty.c('BombDropper', {
+        init: function() {
+            this.requires('Dropper')
 
-        });
+                //Create the bomb
+                .bind('Dropped', function() {
+                    Crafty.e('BananaBomb')
+                        .attr({ col: player.col, row:player.row, z:100})
+            });
+        }
+    });
 
-        Crafty.c('Grid', {  
-            _cellSize: 16,
-            Grid: function(cellSize) {
-                if(cellSize) this._cellSize = cellSize;
-                return this;
-            },
-            init: function() {
-                //Inspired by crafty.js in: Crafty.c("2D", {
-                /*this.props({
-                    col: {
-                        set: function(v) { this.x = this._cellSize * v; },
-                        get: function() { return Math.round(this.x / this._cellSize); } },
-                    row: {
-                        set: function(v) { this.y = this._cellSize * v; },
-                        get: function() { return Math.round(this.y / this._cellSize); }
-                    }*/
-                this.props({
-                    col: { set: function(v) { this.x = this._cellSize * v; }, get: function() { return Math.round(this.x / this._cellSize); } },
-                    row: { set: function(v) { this.y = this._cellSize * v; }, get: function() { return Math.round(this.y / this._cellSize); } }
+    Crafty.c('BananaBomb', {
+        init: function() {
+
+            this.requires("2D, canvas, animate, Grid, banana, explodable")
+                .animate('explode', 4, 0, 5)
+                //.bind('enterframe', function () {
+                //    if (!this.isPlaying())
+                //        this.animate('explode', 50);
+                //})
+                .animate('explode', 50, -1)
+                .delay(function() {
+                    this.trigger("explode");
+                }, 4000)
+                .bind('explode', function() {
+                    this.destroy();
+
+                    //Create fire from the explosion
+                    for(var i = this.col - 2; i < this.col+3; i++)
+                        Crafty.e("BananaFire").attr({ col: i, row: this.row, z:9000 })
+                    for(var i = this.row - 2; i < this.row+3; i++)
+                        Crafty.e("BananaFire").attr({ col: this.col, row: i, z:9000 })
                 });
+        }
+    });
 
-/*
-if(Crafty.support.setter)
-{
-    this.__defineSetter__('col', function(v) { this.x = this._cellSize * v; });
-    this.__defineSetter__('row', function(v) { this.y = this._cellSize * v; });
+    Crafty.c('BananaFire', {
+        init: function() {
+            this.requires("2D, canvas, animate, banana, Grid, collision")
+                .animate('fire', 4, 0, 5)
+                .animate('fire', 10, -1)
+                //.bind('enterframe', function () {
+                //    if (!this.isPlaying())
+                //        this.animate('fire', 10);
+                //})
+                .collision()
+                .onhit('explodable', function(o) {
+                    for(var i = 0; i < o.length; i++) {
+                        o[i].obj.trigger("explode");
+                    }
+                })
+                .delay(function() {
+                    this.destroy();
+                //                  alert("gone " + this.col + " " + this.row);
+                }, 2000);
+        }
+    });
 
-    this.__defineGetter__('col', function() { return Math.round(this.x / this._cellSize); });
-    this.__defineGetter__('row', function() { return Math.round(this.y / this._cellSize); });
-} else if(Crafty.support.defineProperty) {
-    //Object.defineProperty(this, 'col', { set: function(v) { this.x = this._cellSize * v; }, get: function() { return Math.round(this.x / this._cellSize); } });
-    //Object.defineProperty(this, 'row', { set: function(v) { this.y = this._cellSize * v; }, get: function() { return Math.round(this.y / this._cellSize); } });
-} else {
-    // implement 'check on every frame for a difference' fallback
-}
-  */        }
-        });
+    Crafty.c('Grid', {
+        _cellSize: 16,
+        Grid: function(cellSize) {
+            if(cellSize) this._cellSize = cellSize;
+            return this;
+        },
+        init: function() {
+            this.setter('col', function(v) { this.x = this._cellSize * v; }).getter('col', function() { return Math.round(this.x / this._cellSize); })
+                .setter('row', function(v) { this.y = this._cellSize * v; }).getter('row', function() { return Math.round(this.y / this._cellSize); });
+        },
+        snap: function(){
+            this.col = this.col;
+            this.row = this.row;
+        }
+    });
+
+    Crafty.scene("main", function () {
+        generateWorld();
 
         Crafty.c('CustomControls', {
             __move: { left: false, right: false, up: false, down: false },
             _speed: 3,
 
-
+            //TODO: implement direction through cirkular buffer to allow continuous movement after 2. arrow is released and nicer snap.
             CustomControls: function (speed) {
                 if (speed) this._speed = speed;
                 var move = this.__move;
@@ -171,8 +221,9 @@ if(Crafty.support.setter)
                         }).bind('keydown',
                         function (e) {
                             //default movement booleans to false
-                            if(e.keyCode === Crafty.keys.RIGHT_ARROW ||e.keyCode === Crafty.keys.LEFT_ARROW || e.keyCode === Crafty.keys.UP_ARROW || e.keyCode === Crafty.keys.DOWN_ARROW)
-                            move.right = move.left = move.down = move.up = false;
+                            if(e.keyCode === Crafty.keys.RIGHT_ARROW ||e.keyCode === Crafty.keys.LEFT_ARROW || e.keyCode === Crafty.keys.UP_ARROW || e.keyCode === Crafty.keys.DOWN_ARROW){
+                                move.right = move.left = move.down = move.up = false;
+                            }
 
                             //if keys are down, set the direction
                             if (e.keyCode === Crafty.keys.RIGHT_ARROW)  move.right = true;
@@ -187,7 +238,7 @@ if(Crafty.support.setter)
                     if (e.keyCode === Crafty.keys.LEFT_ARROW)   move.left = false;
                     if (e.keyCode === Crafty.keys.UP_ARROW)     move.up = false;
                     if (e.keyCode === Crafty.keys.DOWN_ARROW)   move.down = false;
-
+                    this.snap();
                     //this.preventTypeaheadFind(e);
                 });
 
@@ -196,60 +247,49 @@ if(Crafty.support.setter)
         });
 
         //create our player entity with some premade components
-        player = Crafty.e("2D, canvas, player, controls, CustomControls, animate, collision, Droppable, Grid")
+        player = Crafty.e("2D, canvas, player, controls, CustomControls, animate, collision, BombDropper, Grid")
                 .attr({ x: 160, y: 144, z: 1 })
                 .CustomControls(1)
-                .Droppable()
                 .animate("walk_left", 6, 3, 8)
                 .animate("walk_right", 9, 3, 11)
                 .animate("walk_up", 3, 3, 5)
                 .animate("walk_down", 0, 3, 2)
                 .bind("enterframe",
-                function (e) {
+                    function (e) {
 
-                    if (this.__move.left) {
-                        if (!this.isPlaying("walk_left"))
-                        {
-                            this.stop().animate("walk_left", 10);
+                        if (this.__move.left) {
+                            if (!this.isPlaying("walk_left"))
+                            {
+                                this.stop().animate("walk_left", 10);
+                            }
                         }
-                    }
-                    if (this.__move.right) {
-                        if (!this.isPlaying("walk_right"))
-                            this.stop().animate("walk_right", 10);
-                    }
-                    if (this.__move.up) {
-                        if (!this.isPlaying("walk_up"))
-                            this.stop().animate("walk_up", 10);
-                    }
-                    if (this.__move.down) {
-                        if (!this.isPlaying("walk_down"))
-                            this.stop().animate("walk_down", 10);
-                    }
-                }).bind("keyup", function (e) {
-            this.stop();
-        })
+                        if (this.__move.right) {
+                            if (!this.isPlaying("walk_right"))
+                                this.stop().animate("walk_right", 10);
+                        }
+                        if (this.__move.up) {
+                            if (!this.isPlaying("walk_up"))
+                                this.stop().animate("walk_up", 10);
+                        }
+                        if (this.__move.down) {
+                            if (!this.isPlaying("walk_down"))
+                                this.stop().animate("walk_down", 10);
+                        }
+                })
+                .bind("keyup", function (e) {
+                    this.stop();
+                })
                 .collision()
                 .onhit("wall", function () {
-            if (this.__move.left)
-                this.x += this._speed;
-            if (this.__move.right)
-                this.x -= this._speed;
-            if (this.__move.up)
-                this.y += this._speed;
-            if (this.__move.down)
-                this.y -= this._speed;
-            this.stop();
-        });
-
-            /*.onhit("wall_right", function () {
-             this.x -= this._speed;
-             this.stop();
-             }).onhit("wall_bottom", function () {
-             this.y -= this._speed;
-             this.stop();
-             }).onhit("wall_top", function () {
-             this.y += this._speed;
-             this.stop();
-             })*/;
+                    if (this.__move.left)
+                        this.x += this._speed;
+                    if (this.__move.right)
+                        this.x -= this._speed;
+                    if (this.__move.up)
+                        this.y += this._speed;
+                    if (this.__move.down)
+                        this.y -= this._speed;
+                    this.stop();
+                });
     });
 };
